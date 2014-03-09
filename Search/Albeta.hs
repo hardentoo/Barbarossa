3ac501a13c9ee463109e77fa4f14f6bde633935c
Nicu Ionita <nicu@ionita.at>
2014-03-09 16:44:45 +0100

    Correct node state when generating moves

--- Search/Albeta.hs
+++ Search/Albeta.hs
@@ -665,7 +665,7 @@ pvSearch nst !a !b !d lastnull = do
                 let nst' = if hdeep > 0 && nullSeq (pvcont nst)
                               then nst { pvcont = Seq [e'] }
                               else nst
-                edges <- genAndSort nst0 a b d
+                edges <- genAndSort nst' a b d
                 if noMove edges
                    then do
                      v <- lift staticVal
